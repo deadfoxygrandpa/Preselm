ae5e71833f698c3178e03181e8134ac85efed32b
Alex Neslusan <deadfoxygrandpa@gmail.com>
2014-08-25 14:30:38 +0800

    Added buttons to HelloWorld

--- HelloWorld.elm
+++ HelloWorld.elm
@@ -1,7 +1,18 @@
+import Graphics.Input as Input
+
 import Preselm (..)
+
+input : Input.Input (Maybe Action)
+input = Input.input Nothing
+
+next = Input.button input.handle (Just Forward) "Next!"
+back = Input.button input.handle (Just Backward) "Back!"
+
 
 frame1 = { emptyFrame | middle <- Just [markdown|# Hello World!|] }
 
 frame2 = { emptyFrame | title <- Just "Goodbye", middle <- Just [markdown|#World!|] }
 
-main = presentation [frame1, frame2]
+scene x = layers [flow right [next, back], x]
+
+main = scene <~ (presentation [frame1, frame2] (constant (1000, 1000)) input.signal id)
